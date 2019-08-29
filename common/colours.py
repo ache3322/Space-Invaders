9ec0826d62a00580750d2798bb755b603df91267
Austin <acheese1234@gmail.com>
2019-08-28 23:29:44 -0400

    Added common. Contains colour codes and consts

---
+++ common/colours.py
@@ -0,0 +1,33 @@
+#====================================================================
+# Name: colors.py
+# Created by: Austin Che
+# Modified on: August 28, 2019
+# 
+# Description:
+#   File containing predefined colors
+#====================================================================
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREY = (127, 127, 127)
+
+RED = (255,0,0)
+DRED = (180, 0, 0)
+MAHOGANY = (80, 25, 25)
+
+GREEN = (0, 255 ,0)
+
+BLUE = (0,0,255)
+LIGHTBLUE = (20, 150, 250)
+NIGHT = (65, 25, 90)
+DNIGHT = (23, 17, 56)
+STAR = (237, 237, 97)
+
+BROWN = (170, 100, 50)
+DBROWN = (40, 20, 0)
+HAZY = (45, 25, 21)
+MAHOGANY = (110, 17, 17)
+
+ORANGE = (255, 130, 0)
+YELLOW = (200, 210, 8)
+WINDOW = (158, 158, 16)
---
+++ common/constants.py
@@ -0,0 +1,15 @@
+#====================================================================
+# Name: constants.py
+# Created by: Austin Che
+# Modified on: August 28, 2019
+# 
+# Description:
+#   File containing constants used within project.
+#====================================================================
+
+
+# Dimensions of window width and height
+WIDTH = 800
+HEIGHT = 600
+
+FPS = 60
